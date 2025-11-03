87e5dba972676def100530f51a70d9d5e422aee0
ibai <ikimatarranz24@lhusurbil.eus>
2025-11-03 09:08:57 +0100

    descarga de datos

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os
 import time
 import json
 
-
 config = "config.json"
 datos = "datos"
 
@@ -22,3 +21,29 @@ def guardarConfig(conf):
 
 config  = cargarConfig()
 os.makedirs(datos, exist_ok=True)
+
+
+def descargarDatos():
+    ultimoArchivo = None
+    while True:
+        try: 
+            print("n\Descargando datos de la api")
+            r = requests.get(config["api_url"], timeout= 10)
+
+            if r.status_code == 200:
+                dato = r.json()
+                if dato != ultimoArchivo:
+                    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+                    nombreArchivo = os.path.join(datos, f"contqctos{timestamp}.json")
+                    with open(nombreArchivo, "w") as f:
+                        json.dump(dato, f, indent=4)
+                    ultimoArchivo = dato
+                    print(f"Datos guardados en {nombreArchivo}")
+                else:
+                        print("No hay nuevos datos para guardar.")
+            else:
+                    print(f"Error al descargar datos: {r.status_code}")
+        except Exception as e:  
+            print(f"error {e}")
+
+time.sleep(config["recarga"]) * 60
